b09f7bd3d13fc14d489c8df2311b76f4fab73a23
Lena Ngungu <lenangungu@hotmail.co.uk>
2017-01-16 21:11:27 -0600

    Project completed
    The project is completed but does not have the right items and prices
    yet. IQKeyboarManager cocoa pod used successfully.

---
+++ Pods/Target Support Files/Pods-Wenze/Pods-Wenze-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Wenze : NSObject
+@end
+@implementation PodsDummy_Pods_Wenze
+@end
--- Wenze/AchatViewController.swift
+++ Wenze/AchatViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import MessageUI
 
 class AchatViewController: UIViewController, MFMailComposeViewControllerDelegate {
+    @IBOutlet weak var flagImage: UIImageView!
+    
     @IBOutlet weak var heightC: NSLayoutConstraint!
   
 
@@ -29,8 +31,18 @@ class AchatViewController: UIViewController, MFMailComposeViewControllerDelegate
         super.viewDidLoad()
        
         articleNumber.text = String(arNum!)
-        
-        
+        if (arNum! <= 3)
+        {
+            flagImage.image = UIImage(named: "Flag_of_the_Democratic_Republic_of_the_Congo.svg@1x")
+        }
+        else if (arNum! > 3 && arNum! < 5)
+        {
+        flagImage.image = UIImage(named: "Flag_of_South_Africa.svg")
+        }
+        else
+        {
+             flagImage.image = UIImage(named: "usa.png")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -52,6 +64,22 @@ class AchatViewController: UIViewController, MFMailComposeViewControllerDelegate
     }
     
     @IBAction func sendAction(_ sender: AnyObject) {
+        
+        if (emailTextField.text == "" && numberTextField.text == "")
+        {
+            print ("Need at least you email")
+            let emailController = UIAlertController(title: "Inserez au minimum votre email pour que le vendeur entre en contact avec vous", message: nil, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Ok", style: .cancel){(action) in ()}
+                
+            emailController.addAction(okAction)
+           
+            self.present(emailController, animated: true, completion: nil)
+                
+
+         }
+        
+            
+        else{
         
         if !MFMailComposeViewController.canSendMail()
         {
@@ -75,7 +103,7 @@ class AchatViewController: UIViewController, MFMailComposeViewControllerDelegate
       //  msgVC.body = messageTextView.text + additionalText
        
       // self.present(msgVC, animated: true, completion: nil)
-        
+        }
       
     }
     
